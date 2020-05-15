874607c6835b5c9a20de452cefa01b8d02981f8f
oliverfeher <oliver92.feher@gmail.com>
2020-05-15 00:10:11 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React from "react";
+
+class App extends React.Component
+{
+    constructor()
+    {
+        super();
+        this.state = {
+            humans: "unkown"
+        };
+    }
+
+    componentDidMount()
+    {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(resp=>resp.json())
+        .then(data =>this.setState({
+            humans: data.number
+        }))
+    }
+
+    render()
+    {
+        return <p>There are {this.state.humans} humans in space.</p>
+    }
+
+}
+
+export default App;
